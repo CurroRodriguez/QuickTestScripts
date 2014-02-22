52eb9b607fed44846d51424f838bc7f2907ab970
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2014-02-22 01:36:45 -0500

    Implemented send_mail.py script
    This script test the functionality to send email messages.

--- send_mail.py
+++ send_mail.py
@@ -1,1 +1,17 @@
-print('Hello World')
+import smtplib
+from email.mime.text import MIMEText
+
+server = 'localhost'
+user_name = 'someone@email.com'
+distribution_list = ['first@email.com', 'second@email.com']
+
+msg = MIMEText('Test message send by by Python!')
+msg['Subject'] = 'Python Test Email'
+msg['From'] = user_name
+msg['To'] = '; '.join(distribution_list)
+
+
+server = smtplib.SMTP(server)
+server.sendmail(user_name, distribution_list, msg.as_string())
+
+server.quit()
