ce242e9e9f8e779621147af02ef01f56402c6b40
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2014-03-19 16:23:50 -0400

    Added info on how to test file uploads.

--- file_upload.py
+++ file_upload.py
@@ -1,3 +1,14 @@
+'''
+Testing:
+
+In order to test this script, run it through the debugger or by invoking
+python file_upload.py. With the local server running, you can use curl.exe
+to post a file (expecting a ZIP file) with the following command line::
+
+    curl.exe -X POST -H "Content-Type: application/octet-stream" --data-binary "@large_test.zip" http://localhost:8080
+
+Notice the file name is surrounded by quotes and prefixed with an '@' sign.
+'''
 import cherrypy 
 import shutil
 
@@ -5,9 +16,12 @@ import shutil
 class UploadHandler(object):
 
     def __call__(self, *args, **kwargs):
-        fout = file('read.zip', mode='w+b')
-        with fout:
-            fout.write(cherrypy.request.rfile.read())
+        with file('result.zip', mode='w+b') as fout:
+            #body = cherrypy.request.rfile
+            body = cherrypy.request.body
+            if body:
+                fout.write(body.read())
+
 
         return 'Success!!!'
 
