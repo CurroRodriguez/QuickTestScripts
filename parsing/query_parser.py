8de621faa12709b48a5eb95369224b211e8296ee
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2014-04-25 15:39:50 -0400

    Added tests to support numeric values.

--- parsing/query_parser.py
+++ parsing/query_parser.py
@@ -1,4 +1,5 @@
 import pyparsing as pp
+import string
 
 
 
@@ -7,9 +8,42 @@ class Parser(object):
     def parse(self, expression_string):
         identifier = pp.Word(pp.alphanums)
         operators = pp.Regex('=')
+        operator_tokens = pp.Regex('\$eq')
+        comparison_operators = operators | operator_tokens
         string_value = pp.QuotedString('"')
         numeric_value = pp.Regex(r"[+-]?\d+(:?\.\d*)?(:?[eE][+-]?\d+)?")
-        constant = string_value | numeric_value
-        expression = pp.Group(identifier + operators + constant)
+        boolean_value = pp.Regex('true')
+        constant = string_value | numeric_value | boolean_value
+        constant.setParseAction(self.replace_if_numeric, self.replace_if_boolean)
+        expression = pp.Group(identifier + comparison_operators + constant)
         result = expression.parseString(expression_string)
         return result.asList()
+
+
+    def replace_if_numeric(self, s=None, loc=None, toks=None):
+        if toks:
+            maybe_numeric = try_convert_numeric(toks[0])
+            return [maybe_numeric]
+
+
+    def replace_if_boolean(self, s=None, loc=None, toks=None):
+        if toks:
+            value = toks[0]
+            if value in ['true']:
+                return [True]
+
+
+    
+
+
+
+
+def try_convert_numeric(s):
+    try:
+        return float(s)
+        return f if str(f) == s else int(s)
+    except ValueError:
+        try:
+            return int(s)
+        except ValueError:
+            return s
--- parsing/test_query_parser.py
+++ parsing/test_query_parser.py
@@ -15,9 +15,29 @@ class TestParser(unittest.TestCase):
         self.expect(['id', '=', 'id'])
 
 
-    def test_parses_numeric_filter(self):
+    def test_parses_integer_values(self):
         self.given('id=1')
-        self.expect(['id', '=', '1'])
+        self.expect(['id', '=', 1])
+
+
+    def test_parses_floats(self):
+        self.given('elevation=2.345')
+        self.expect(['elevation', '=', 2.345])
+
+
+    def test_parses_negative_floats(self):
+        self.given('elevation=-3.21')
+        self.expect(['elevation', '=', -3.21])
+
+
+    def test_parses_boolean_value(self):
+        self.given('visible=true')
+        self.expect(['visible', '=', True])
+
+
+    def test_parses_equality_operator(self):
+        self.given('id$eq"id"')
+        self.expect(['id', '$eq', 'id'])
 
 
 
@@ -29,6 +49,49 @@ class TestParser(unittest.TestCase):
         expected_list = [expected]
         actual = self.parser.parse(self.expression)
         self.assertListEqual(actual, expected_list)
+
+
+
+class TestTryConvertNumeric(unittest.TestCase):
+
+    def test_returns_string_if_not_numeric(self):
+        self.given('asdf')
+        self.expect('asdf')
+
+
+    def test_returns_integer_if_integer_string(self):
+        self.given('1')
+        self.expect(1)
+
+
+    def test_returns_negative_integer(self):
+        self.given('-3')
+        self.expect(-3)
+
+
+    def test_returns_float_if_float_string(self):
+        self.given('1.2')
+        self.expect(1.2)
+
+
+    def test_returns_negative_float(self):
+        self.given('-1.2')
+        self.expect(-1.2)
+
+
+    def test_returns_float_with_precision(self):
+        self.given('1.10')
+        self.expect(1.1)
+
+
+
+    def given(self, s):
+        self.str_value = s
+
+
+    def expect(self, expected):
+        actual = query_parser.try_convert_numeric(self.str_value)
+        self.assertEqual(actual, expected)
 
 
 
